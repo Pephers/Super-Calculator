5b562bd88878edcba6b6fe0be2274a52c352ef84
Chiel Robben <pephers@gmail.com>
2012-07-03 20:59:41 +0200

    moved find_reverse logic into seperate function

--- Super Calculator.py
+++ Super Calculator.py
@@ -3,34 +3,37 @@ import re
 
 class SuperCalculatorCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-            regex = '(?=[0-9]+)[0-9+\-*/.\(\)]+'
-            selected_regions = self.view.sel()
-            for region in selected_regions:
-                expr = self.view.substr(region)
-                if re.match(regex, expr):
-                    # calculate expression and replace it with the result
-                    result = str(eval(expr))
-                    self.view.replace(edit, region, result)
-                    # move cursor after the result
-                    result_region = self.view.sel()[-1]
-                    result_region_end = sublime.Region(result_region.end(), result_region.end())
-                    self.view.sel().clear()
-                    self.view.sel().add(result_region_end)
-                    sublime.status_message("Calculated result: " + expr + "=" + result)
-                else:
-                    line_region = self.view.line(region)
-                    # do reverse find
-                    new_regions = (r for r in reversed(self.view.find_all(regex))
-                        if r.begin() < region.end())
-                    try:
-                        match_region = new_regions.next()
-                    except StopIteration:
-                        continue
-                    if match_region:
-                        match = self.view.substr(match_region)
-                        # validate result and check if it is in the current line
-                        if re.match(regex, match) and line_region.begin() <= match_region.begin():
-                            self.view.sel().clear()
-                            self.view.sel().add(match_region)
-                            print match_region
-                            sublime.status_message("Calculate: " + match + "?")
+        regex = '(?=[0-9]+)[0-9+\-*/.\(\)]+'
+        selected_regions = self.view.sel()
+        for region in selected_regions:
+            expr = self.view.substr(region)
+            if re.match(regex, expr):
+                # calculate expression and replace it with the result
+                result = str(eval(expr))
+                self.view.replace(edit, region, result)
+                # move cursor after the result
+                result_region = self.view.sel()[-1]
+                result_region_end = sublime.Region(result_region.end(), result_region.end())
+                self.view.sel().clear()
+                self.view.sel().add(result_region_end)
+                sublime.status_message("Calculated result: " + expr + "=" + result)
+            else:
+                line_region = self.view.line(region)
+                match_region = self.find_reverse(regex, region)
+                if match_region:
+                    match = self.view.substr(match_region)
+                    # validate result and check if it is in the current line
+                    if re.match(regex, match) and line_region.begin() <= match_region.begin():
+                        self.view.sel().clear()
+                        self.view.sel().add(match_region)
+                        sublime.status_message("Calculate: " + match + "?")
+
+    def find_reverse(self, string, region):
+        new_regions = (r for r in reversed(self.view.find_all(string))
+            if r.begin() < region.end())
+        try:
+            new_region = new_regions.next()
+        except StopIteration:
+            return None
+        else:
+            return new_region
